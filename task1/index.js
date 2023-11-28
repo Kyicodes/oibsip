bf2b9848129b437ce4e93cfdb872b42ee9094277
Kyicodes <140958930+Kyicodes@users.noreply.github.com>
2023-11-28 13:37:35 -0800

    Created initial index.js file

---
+++ task1/index.js
@@ -0,0 +1,7 @@
+let numberButtons = document.getElementById("number");
+let operationButtons = document.getElementById("operation");
+let deleteButton = document.getElementById("delete");
+let equalsButton = document.getElementById("equals");
+let clearButton = document.getElementById("allClear");
+let previousText = document.getElementById("previousinput");
+let currentText = document.getElementById("currentinput");
