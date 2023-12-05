b3d7c34c771708e486c861804efbdc4c9657cb5e
Kyicodes <140958930+Kyicodes@users.noreply.github.com>
2023-12-04 20:31:56 -0800

    added submit button

--- task3/index.js
+++ task3/index.js
@@ -2,6 +2,7 @@ let todoInput = document.getElementById("todo-input");
 let listItems = document.getElementById("list-items");
 let addTaskButton = document.getElementById("addButton");
 
+// Data creation
 function createToDoData() {
   if (todoInput.value === "") {
     todoInput.focus();
@@ -12,34 +13,72 @@ function createToDoData() {
 
 addTaskButton.addEventListener("click", createToDoData);
 
+// Add items
 function addItemsToList() {
-  let newItem = document.createElement("li");
-  newItem.innerText = todoInput.value;
+  let listItem = document.createElement("li");
+
+  let todoText = document.createElement("span");
+  todoText.innerText = todoInput.value;
+  listItem.appendChild(todoText);
 
   // Edit button
   let editButton = document.createElement("button");
   editButton.innerText = "✏";
-  editButton.addEventListener("click", () => {
-    let editInput = document.createElement("input");
-    editInput.value = newItem.innerText;
-    newItem.innerText = "";
-    newItem.appendChild(editInput);
-    editInput.focus();
-    editInput.addEventListener("blur", () => {
-      newItem.innerText = editInput.value;
-    });
-  });
-  newItem.appendChild(editButton);
+  listItem.appendChild(editButton);
 
   // Delete button
   let deleteButton = document.createElement("button");
   deleteButton.innerText = "✖";
-  deleteButton.addEventListener("click", () => {
-    newItem.remove();
-  });
-  newItem.appendChild(deleteButton);
+  listItem.appendChild(deleteButton);
 
-  listItems.appendChild(newItem);
+  let editInput = document.createElement("input");
+  editInput.style.display = "none";
+  listItem.appendChild(editInput);
+
+  // Submit button
+  let submitButton = document.createElement("button");
+  submitButton.innerText = "✔";
+  submitButton.style.display = "none";
+  listItem.appendChild(submitButton);
+
+  function showEditInput() {
+    editInput.value = todoText.innerText;
+    todoText.style.display = "none";
+    editInput.style.display = "inline-block";
+    submitButton.style.display = "inline-block";
+    editInput.focus();
+  }
+
+  function hideEditInput() {
+    todoText.style.display = "inline-block";
+    editInput.style.display = "none";
+    submitButton.style.display = "none";
+  }
+
+  editButton.addEventListener("click", (event) => {
+    if (listItem.classList.contains("editing")) {
+      return;
+    }
+
+    listItem.classList.add("editing");
+
+    showEditInput();
+  });
+
+  submitButton.addEventListener("click", () => {
+    let editedText = editInput.value.trim();
+    todoText.innerText = editedText;
+    listItem.classList.remove("editing");
+    editButton.innerText = "✏";
+
+    hideEditInput();
+  });
+
+  deleteButton.addEventListener("click", () => {
+    listItem.remove();
+  });
+
+  listItems.appendChild(listItem);
   todoInput.value = "";
   todoInput.focus();
 }
